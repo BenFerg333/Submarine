eafcd87f18dbf38957f64f5e9665fb6018276d54
BenFerg333 <benferg297@gmail.com>
2024-04-21 22:05:23 -0700

    Began Calculations

--- submarine.js
+++ submarine.js
@@ -4,6 +4,7 @@ var columns = 0;
 var boardDims = 5;
 var arrows = document.getElementsByClassName("arrowList");
 var moves = [];
+var possiblePositions = []
 
 //create board
 createBoard(boardDims);
@@ -14,6 +15,7 @@ var board = $(".cell");
 addIce(getCellID(2,2));
 addIce(getCellID(2,5));
 addIce(getCellID(5,3));
+checkPositions();
 
 //add user input
 document.body.addEventListener("keydown", checkInput);
@@ -22,18 +24,22 @@ function checkInput(){
 	if(key == "n" || key == "arrowup"){
 		moves.push("n");
 		addMove("N");
+		checkMove("n");
 	}
 	if(key == "e" || key == "arrowright"){
 		moves.push("e");
 		addMove("E");
+		checkMove("e");
 	}
 	if(key == "s" || key == "arrowdown"){
 		moves.push("s");
 		addMove("S");
+		checkMove("s");
 	}
 	if(key == "w" || key == "arrowleft"){
 		moves.push("w");
 		addMove("W");
+		checkMove("w");
 	}
 }
 function addMove(direction){
@@ -41,11 +47,13 @@ function addMove(direction){
 	text.innerHTML += direction + ", ";
 }
 $("#delete").on("click", function(){
-	console.log($("#movesText").innerHTML);
-	$("#movesText").innerHTML = $("#movesText").innerHTML.substring(0, $("#movesText").innerHTML.length - 3);
+	let movesText = $("#movesText").text();
+	if(movesText.length > 13){
+		$("#movesText").text(movesText.substring(0, movesText.length - 3));
+	}
 });
 $("#clear").on("click", function(){
-
+	$("#movesText").text("Enemy Moves: ");
 });
 function addIce(pos){
 	board[pos].classList.add("ice");
@@ -63,6 +71,10 @@ function createBoard(dimensions){
 	document.body.appendChild(boardEl);
 	for(let i = 0; i < dimensions; i++){
 		createRow(boardEl, dimensions);
+
+	}
+	for(let i = 0; i < 25; i++){
+		possiblePositions.push(i);
 	}
 }
 function createRow(parent, length){
@@ -74,4 +86,47 @@ function createRow(parent, length){
 		el.classList.add("cell");
 		rowEl.appendChild(el);
 	}
+}
+
+//calculations
+function checkPositions(){
+	for (var i = 0; i < possiblePositions.length; i++) {
+		console.log("Checking cell index " + possiblePositions[i]);
+		if($(".cell")[possiblePositions[i]].classList.contains("ice")){
+			possiblePositions.splice(i, 1); //removes index with ice
+			i--;
+		}
+		else{
+			$(".cell")[possiblePositions[i]].classList.add("target");
+		}
+	}
+	console.log(possiblePositions);
+}
+function checkMove(direction){
+	let modifier = 0;
+	if(direction == "e"){modifier = 1}
+	if(direction == "w"){modifier = -1}
+	if(direction == "s"){modifier = boardDims}
+	if(direction == "n"){modifier = -boardDims}
+
+	for(var i = 0; i < possiblePositions.length; i++){
+		$(".cell")[possiblePositions[i]].classList.remove("target");
+		possiblePositions[i] += modifier;
+		if(possiblePositions[i] >= boardDims*boardDims){
+			console.log(possiblePositions[i] + " deleted due to condition 1");
+			possiblePositions.splice(i, 1);
+		}
+		else if(possiblePositions[i] < 0){
+			console.log(possiblePositions[i] + " deleted due to condition 2");
+			possiblePositions.splice(i, 1);
+		}
+		else if(possiblePositions[i] / boardDims != (possiblePositions[i] - modifier) / boardDims){
+			console.log(possiblePositions[i] + " " + boardDims);
+			possiblePositions.splice(i, 1);
+		}
+		else{
+			$(".cell")[possiblePositions[i]].classList.add("target");
+		}
+	};
+	//left and right: column change %boardDims !=
 }
